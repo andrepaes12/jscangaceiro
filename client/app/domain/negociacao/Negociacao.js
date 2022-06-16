d4c62f44a08a6b93fb9a52d269e0d5bf584a0ea6
André Paes <aluispaes@gmail.com>
2022-06-16 14:30:13 -0300

    protegendo os dados contra alterações, usando recursos dentro do contructor()

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -3,7 +3,9 @@ class Negociacao{
   constructor(data, quantidade, valor){ //parâmetros do construtor
     // this.data = new Date(); //data atual
     // uso do _ para lembrar que a propriedade não deve ser alterada
-    this._data = data;
+    // this._data = data;
+    // para data não ser alterada
+    this._data = new Date(data.getTime());
     this._quantidade = quantidade;
     this._valor = valor;
     Object.freeze(this);
@@ -14,7 +16,9 @@ class Negociacao{
   }
 
   get data(){
-    return this._data;
+    // return this._data;
+    // evitar alterar a data
+    return new Date(this._data.getTime());
   }
 
   get quantidade(){
