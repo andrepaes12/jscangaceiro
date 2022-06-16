7619050f17c48a44ef360194e5338766204b1726
André Paes <aluispaes@gmail.com>
2022-06-16 13:45:04 -0300

    usando a propriedade get no método

--- client/app/domain/negociacao/Negociacao.js
+++ client/app/domain/negociacao/Negociacao.js
@@ -2,12 +2,25 @@ class Negociacao{
   // definir a propriedade da classe
   constructor(data, quantidade, valor){ //parâmetros do construtor
     // this.data = new Date(); //data atual
-    this.data = data;
-    this.quantidade = quantidade;
-    this.valor = valor;
+    // uso do _ para lembrar que a propriedade não deve ser alterada
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
   }
 
-  obtemVolume(){
-    return this.quantidade * this.valor;
+  get volume(){
+    return this._quantidade * this._valor;
+  }
+
+  get data(){
+    return this._data;
+  }
+
+  get quantidade(){
+    return this._quantidade;
+  }
+
+  get valor(){
+    return this._valor;
   }
 }
