fdf232b9af5714ed0443e3ac92b2d9079947ed7b
André Paes <aluispaes@gmail.com>
2022-06-17 10:59:55 -0300

    fail-fast ; regEx

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -10,6 +10,11 @@ class DateConverter {
   }
 
   static paraData(texto){
+    // fail-fast
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(texto)){ // o uso dos {} é opcional p/ 1 condição
+      // requer identação quando não usar os {}
+      throw new Error('Deve estar no formato aaaa-mm-dd');
+    }
     return new Date(...texto.split('-')
     .map((item, indice) => item - indice % 2));
   }
